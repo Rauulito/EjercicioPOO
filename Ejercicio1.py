0fbf28147ffdd0aa95086c0d6fc9afb0f9db46f2
Rauulito <7rnavarro7@gmail.com>
2021-11-14 18:43:53 +0100

    realizado

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -23,17 +23,17 @@ class Libro():
         self.genero= genero
     def setNPaginas(self,nPaginas):
         self.nPaginas= nPaginas
-    def getTitulo(self,titulo):
+    def getTitulo(self):
         return self.titulo
-    def getAutor(self,autor):
+    def getAutor(self):
         return self.autor
     def getEditorial(self,editorial):
         return self.editorial
     def getPrecioBase(self,precioBase):
         return self.precioBase
-    def getGenero(self,genero):
+    def getGenero(self):
         return self.genero
-    def getNPaginas(self,nPaginas):
+    def getNPaginas(self):
         return self.nPaginas
 
 libro =  Libro("El Palomo","Carlos Rodriguez","Edelvives","180","Aventuras","330")
