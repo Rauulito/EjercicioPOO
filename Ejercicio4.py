5ae1598186cde176c04428a525154f2a7effd26a
Rauulito <7rnavarro7@gmail.com>
2021-11-14 18:44:12 +0100

    realizado

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -14,13 +14,11 @@ class CuentaBanacaria():
         self.numeroCuenta= numeroCuenta
         self.saldo= saldo
 
+    #Metodo para obtener el saldo lo utilizaremos para ver si se puede sacar dinero y tranferir dinero
+    def getSaldo(self):
+            return self.saldo
+
     #Metodo para sacar
     def sacarDinero(self, dineroSacado):
-        self.saldo=  self.saldo- dineroSacado
-
-    #Metodo para meter
-    def meterDinero(self, dineroMetido):
-        self.saldo=  self.saldo + dineroMetido
-
-    #Metodo para traspasar
-    def traspasarDinero(self,dinero,cuenta1,cuenta2):
+        if(getSaldo> dineroSacado):
+            self.saldo= self.saldo - dineroSacado
