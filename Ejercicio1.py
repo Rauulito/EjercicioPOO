e5a388acfa23cadedc97f914f23575aed093c37e
Rauulito <7rnavarro7@gmail.com>
2021-11-14 13:13:34 +0100

    realizado

---
+++ Ejercicio1.py
@@ -0,0 +1,13 @@
+class Libro():
+
+    #Creamos el constructor
+    def __init__(self, nombre,autor,editorial,precioBase, genero, nPaginas):
+        self.nombre = nombre
+        self.autor= autor
+        self.editorial= editorial
+        self.precioBase= precioBase
+        self.genero= genero
+        self.nPaginas= nPaginas
+
+    #Probamos que funciona
+    
