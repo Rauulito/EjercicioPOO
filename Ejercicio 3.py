abd0f16660588863f846d68056ad3b5792dff73a
Rauulito <7rnavarro7@gmail.com>
2021-11-14 17:42:21 +0100

    realizado

---
+++ Ejercicio 3.py
@@ -0,0 +1,12 @@
+class Animal():
+    name=""
+class Mamifero():
+    edad= ""
+class Oviparo():
+    edad= ""
+class Pollo():
+    hijos= 0
+class Gato():
+    vidas=7
+class Ornitorricno():
+    hermanos=2
